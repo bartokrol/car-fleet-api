e51835d52ecdf05f80a8146b1f085c7fab5e8d4f
Bartosz Król <barto.kro@gmail.com>
2021-09-27 12:07:15 +0200

    create do while loop that adds a specific count of cars into fetchedCars arr and setCars with fetchedCars value

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import { useEffect, useState } from "react";
 
+const carsCount = 20;
+
 function App() {
 	const [cars, setCars] = useState([]);
-	
+
 	useEffect(() => {
-		console.log("loaded");
+		const fetchDrivers = () => {
+			const fetchedCars = [];
+			let carId = 0;
+			do {
+				fetchedCars.push({
+					carId,
+				});
+				carId++;
+			} while (fetchedCars.length < carsCount);
+			if (fetchedCars.length === carsCount) {
+				setCars(fetchedCars);
+			}
+		};
+		fetchDrivers();
 	}, []);
 	return <div className="App"></div>;
 }
