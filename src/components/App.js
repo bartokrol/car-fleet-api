eee74701c5a99556f974d90b5d80c0f6a1ce675e
Bartosz Król <barto.kro@gmail.com>
2021-09-27 12:15:23 +0200

    create fetchLicense function

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,33 @@
 import { useEffect, useState } from "react";
 
 const carsCount = 20;
+const letters = "ABCDEFZGHIJKLMNOPQRSTWXYZ";
+const licenseNumberCount = 3;
 
 function App() {
 	const [cars, setCars] = useState([]);
 
 	useEffect(() => {
+		const fetchLicense = (number) => {
+			let carLicenseLetters = "";
+			let carLicenseNumbers = "";
+			for (let i = 0; i < number; i++) {
+				carLicenseLetters +=
+					letters[Math.floor(Math.random() * letters.length)];
+				carLicenseNumbers += Math.floor(Math.random() * 10);
+			}
+			const licenseNumber = `${carLicenseLetters} ${carLicenseNumbers}`;
+			return licenseNumber;
+		};
+
 		const fetchDrivers = () => {
 			const fetchedCars = [];
 			let carId = 0;
 			do {
+				const licenseNumber = fetchLicense(licenseNumberCount);
 				fetchedCars.push({
 					carId,
+					licenseNumber,
 				});
 				carId++;
 			} while (fetchedCars.length < carsCount);
