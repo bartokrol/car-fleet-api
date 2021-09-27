1bd0dbf3648e99401612233a023c7b527d7842f7
Bartosz Król <barto.kro@gmail.com>
2021-09-27 17:36:10 +0200

    create mapCars function, and create favoriteCarsFilter and normalCarsFilter variables

--- src/layouts/Cars.js
+++ src/layouts/Cars.js
@@ -2,15 +2,25 @@ import Car from "./Car";
 
 const Cars = ({ carsClass, cars, favoriteChange }) => {
 	const carsClassName = `${carsClass}__cars`;
-	const carsFiltered = cars.map((car) => (
-		<Car
-			key={car.carId}
-			carClass={carsClassName}
-			car={car}
-			favoriteChange={favoriteChange}
-		/>
-	));
-	return <div className={carsClassName}>{carsFiltered}</div>;
+	const mapCars = (filterArr) => {
+		return filterArr.map((car) => (
+			<Car
+				key={car.carId}
+				carClass={carsClassName}
+				car={car}
+				favoriteChange={favoriteChange}
+			/>
+		));
+	};
+
+	const favoriteCarsMap = mapCars(cars.filter((car) => car.favorite));
+	const normalCarsMap = mapCars(cars.filter((car) => !car.favorite));
+	return (
+		<div className={carsClassName}>
+			{favoriteCarsMap}
+			{normalCarsMap}
+		</div>
+	);
 };
 
 export default Cars;
